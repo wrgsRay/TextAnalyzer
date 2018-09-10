4d8fbfb9c7900ef7d445d874ed05b37470445f97
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-10 15:57:28 -0700

    refactor into class

--- main.py
+++ main.py
@@ -5,30 +5,45 @@ Python 3.6
 from tkinter import *
 
 
+class Analyzer(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        # Set title and size
+        master.title('Analyze this')
+        master.geometry('900x600')
+
+        # Set label
+        self.label_text = StringVar()
+        self.top_label = Label(master, textvariable=self.label_text, bg='gray', font=('Arial', 12),
+                               width=15, height=2)
+        self.top_label.pack()
+
+        # Set input box
+        self.message_box = Entry(master, width=100)
+        self.message_box.focus()
+        self.message_box.pack()
+
+        # Set Submit button
+        self.submit_button = Button(master, text='Submit', width=15, height=2, command=self.submit)
+        self.submit_button.pack()
+
+        # Set Reset button
+        self.reset_button = Button(master, text='Reset', width=15, height=2, command=self.reset)
+        self.reset_button.pack()
+
+    def submit(self):
+        self.label_text.set('{} digits long'.format(str(len(self.message_box.get()))))
+
+    def reset(self):
+        self.label_text.set('')
+        self.message_box.delete(0, 'end')
+
+
 def main():
-
-    def submit():
-        label_text.set(f'{str(len(message_box.get()))} digits long')
-
     root = Tk()
-    # Set title
-    root.title('Analyze this')
-
-    # Set size
-    root.geometry('900x600')
-    label_text = StringVar()
-    # Set label
-    top_label = Label(root, textvariable=label_text, bg='gray', font=('Arial', 12), width=15, height=2)
-    top_label.pack()
-
-    # Set input box
-    message_box = Entry(root, width=100)
-    message_box.pack()
-    # Set button
-    submit_button = Button(root, text='Submit', width=15, height=2, command=submit)
-    submit_button.pack()
-
     # mainloop
+    Analyzer(root).pack(expand=True, fill='both')
     root.mainloop()
 
 
