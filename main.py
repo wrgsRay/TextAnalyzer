0aa7708142593f043c50277ac73d4d5e8eea91f0
Raymond at Work <28991499+wrgsRay@users.noreply.github.com>
2018-09-10 16:32:09 -0700

    layout by grid

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ Python 3.6
 @Author: wrgsRay
 """
 from tkinter import *
+import pyperclip
 
 
 class Analyzer(Frame):
@@ -11,26 +12,33 @@ class Analyzer(Frame):
         self.master = master
         # Set title and size
         master.title('Analyze this')
-        master.geometry('900x600')
+        master.geometry('700x300')
+        # Set padding
+        self.paddingx = 50
+        self.paddingy = 10
 
         # Set label
         self.label_text = StringVar()
         self.top_label = Label(master, textvariable=self.label_text, bg='gray', font=('Arial', 12),
                                width=15, height=2)
-        self.top_label.pack()
+        self.top_label.grid(row=0, column=0, columnspan=3, padx=self.paddingx, pady=self.paddingy)
 
         # Set input box
         self.message_box = Entry(master, width=100)
         self.message_box.focus()
-        self.message_box.pack()
+        self.message_box.grid(row=1, column=0, columnspan=3, padx=self.paddingx, pady=self.paddingy)
 
         # Set Submit button
         self.submit_button = Button(master, text='Submit', width=15, height=2, command=self.submit)
-        self.submit_button.pack()
+        self.submit_button.grid(row=3, column=0)
+
+        # Set Copy button
+        self.copy_button = Button(master, text='Copy to Clipboard', width=15, height=2, command=self.copy)
+        self.copy_button.grid(row=3, column=1)
 
         # Set Reset button
         self.reset_button = Button(master, text='Reset', width=15, height=2, command=self.reset)
-        self.reset_button.pack()
+        self.reset_button.grid(row=3, column=2)
 
     def submit(self):
         self.label_text.set('{} digits long'.format(str(len(self.message_box.get()))))
@@ -39,11 +47,14 @@ class Analyzer(Frame):
         self.label_text.set('')
         self.message_box.delete(0, 'end')
 
+    def copy(self):
+        pyperclip.copy(self.message_box.get())
+
 
 def main():
     root = Tk()
     # mainloop
-    Analyzer(root).pack(expand=True, fill='both')
+    Analyzer(root)
     root.mainloop()
 
 
